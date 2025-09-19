ad183f9cf8911c2b3fe8810e7f4367e0e7ccfc4d
LICO-m <yokomaeda928@gmail.com>
2025-09-19 14:55:45 +0900

    課題020の追加

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const clickBtn = document.getElementById('btn');
+const newText = document.getElementById('text')
+clickBtn.addEventListener('click', () => {
+newText.textContent = "ボタンをクリックしました";
+});
